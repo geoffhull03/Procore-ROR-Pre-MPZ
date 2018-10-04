49d38eca69eea6ef1fcd54b6b308e44db327c609
Geoff Hull <geoffhull03@gmail.com>
2018-10-04 15:52:28 -0700

    Fixed wrong redirect in projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,7 @@ class ProjectsController < ApplicationController
     @company_id = params["company_id"]
 
     if @company_id == "" || @company_id.blank?
-        redirect_to welcome_sign_in_path
+        redirect_to welcome_home_page_path
     else
         #This API request must be changed to the root "api.procore.com and contain the Procore-Company-ID header""
         projects_response = RestClient.get("https://app.procore.com/vapid/projects?company_id=#{@company_id}", {"Authorization": "Bearer #{session[:access_token]}"})
